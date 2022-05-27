6e59cbdaf246590d4164b7720f26b9ee93e6e1e0
vapb <vapb7123@gmail.com>
2022-05-27 12:01:53 -0300

    JoiningSql 25%

---
+++ Joining_SQL.sql
@@ -0,0 +1,40 @@
+-- InnerJoin 
+SELECT c.code,
+       name,
+       region,
+       e.year,
+       fertility_rate,
+       unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code AND p.year = e.year;
+
+-- InnerJoin USING
+SELECT c.name AS country,
+       continent,
+       l.name AS language,
+       l.official
+FROM countries AS c
+INNER JOIN languages as l
+USING (code)
+
+-- SelfJoin
+SELECT p1.country_code,
+       p1.size AS size2010,
+       p2.size AS size2015
+FROM populations as p1
+INNER JOIN populations as p2
+ON p1.country_code = p2.country_code AND p1.year = (p2.year - 5) 
+
+-- Case When then
+SELECT name, continent, code, surface_area,
+CASE 
+    WHEN surface_area > 2000000 THEN 'large'
+    WHEN surface_area > 350000 THEN 'medium'
+    ELSE 'small' END
+    AS geosize_group
+FROM countries;
+
+-- INto 
