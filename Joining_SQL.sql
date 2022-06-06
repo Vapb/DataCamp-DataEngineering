ab0e76f3c627e2b5bc5a68e14400653912014643
vapb <vapb7123@gmail.com>
2022-06-06 16:13:06 -0300

    JoiningSQL 100%

--- Joining_SQL.sql
+++ Joining_SQL.sql
@@ -59,3 +59,64 @@ FROM economies
 SELECT country_code, year
 FROM populations
 ORDER BY code, year;
+
+-- Intersect
+SELECT code, year
+FROM economies
+    INTERSECT  
+SELECT country_code, year
+FROM populations
+ORDER BY code, year;
+
+-- Except
+SELECT name
+FROM cities
+	EXCEPT
+SELECT capital
+FROM countries
+ORDER BY name;
+
+-- Subqueries / nested queries And interesting nested positions
+SELECT name, country_code, urbanarea_pop
+FROM cities
+WHERE name IN
+  (select capital from countries)
+ORDER BY urbanarea_pop DESC;
+
+SELECT countries.name AS country,
+  (SELECT  COUNT(*)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
+select countries.local_name , subquery.lang_num
+from countries,
+  	(SELECT code, COUNT(*) AS lang_num
+  	 FROM languages
+  	 GROUP BY code) AS subquery
+  where countries.code = subquery.code
+order by lang_num desc;
+
+
+SELECT code, inflation_rate, unemployment_rate
+FROM economies
+WHERE year = 2015 AND code not in
+    (SELECT code
+     FROM countries
+     WHERE (gov_form = 'Constitutional Monarchy' OR gov_form LIKE '%Republic%'))
+ORDER BY inflation_rate;
+
+
+SELECT DISTINCT c.name, e.total_investment, e.imports
+  FROM countries AS c
+    LEFT JOIN economies AS e
+      ON (c.code = e.code
+        AND c.code IN (
+          SELECT l.code
+          FROM languages AS l
+          WHERE official = 'true'
+        ) )
+  WHERE region = 'Central America' AND year = 2015
+ORDER BY c.name;
